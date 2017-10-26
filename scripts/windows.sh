8b81c131e5addfe0ba7d4cd766b657ed3b57e61e
Leonardo <modlfo@gmail.com>
2017-10-26 17:59:03 +0300

    Test windows ci

---
+++ scripts/windows.sh
@@ -0,0 +1,17 @@
+pacman -S -yy --noconfirm git make tar zip unzip mingw-w64-x86_64-gcc mingw-w64-x86_64-cmake
+mkdir build
+cd build
+git clone https://github.com/VCVRack/Rack.git
+cd Rack
+git submodule update --init --recursive
+make dep
+make -j 4
+cd plugins
+git clone https://github.com/modlfo/VultModules.git
+cd VultModules
+make
+make dist
+cd dist
+zip VultModules.zip ./VultModules/*
+cp VultModules.zip ../../../../VultModules.zip
+cd ../../../../
